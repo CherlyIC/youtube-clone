f4ecf217a7de3cb149d9e3ef76e2d66c9e6628d3
CherlyIC <cherlyimfurankunda@gmail.com>
2026-03-15 16:16:55 +0200

    feat: setup routes and QueryClient in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
+import React from 'react'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { useState } from 'react'
+import Feed from './pages/Feed'
+import VideoDetails from './pages/VideoDetails'
+import ChannelDetails from './pages/ChannelDetails'
+import SearchResults from './pages/SearchResults'
+
+
+const queryClient = new QueryClient()
 
 function App() {
-  const queryClient = new QueryClient()
+  const [ selectedCategory, setSelectedCategory] = useState('New')
+
   return(
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <div className="bg-black min-h-screen text-white">
           <Routes>
+             <Route
+              path="/"
+              element={
+                <Feed
+                  selectedCategory={selectedCategory}
+                  setSelectedCategory={setSelectedCategory}
+                />
+              }
+            />
+            <Route path="/video/:id" element={<VideoDetails />} />
+            <Route path="/channel/:id" element={<ChannelDetails />} />
+             <Route path="/search/:searchTerm" element={<SearchResults />} />
+
+
+
+            
 
           </Routes>
         </div>
